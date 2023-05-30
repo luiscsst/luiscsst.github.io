26b3da3c7893302902ee8b40ffbcfa732fab3e49
luiscsst <124744464+luiscsst@users.noreply.github.com>
2023-05-29 17:49:26 -0700

    Add files via upload

---
+++ parcial3/practica2.php
@@ -0,0 +1,43 @@
+<?php
+
+$nombre = $_GET["nombre"];
+
+echo "Nombre: ".$nombre ."<br><br>";
+
+
+
+$edad = $_GET["edad"];
+
+echo "Edad: ".$edad ."<br><br>";
+
+$escuela = $_GET["escuela"];
+
+echo "Escuela: ".$escuela ."<br><br>";
+
+$fecha_ingreso = $_GET["fecha_ingreso"];
+
+echo "Fecha de ingreso: ".$fecha_ingreso ."<br><br>";
+
+$direccion = $_GET["direccion"];
+
+echo "Direccion: ".$direccion ."<br><br>";
+
+if($edad >= 18){
+echo "Es MAYOR de edad" . "<br><br>";
+}else{
+    echo"es MENOR de edad" . "<br><br>";
+}
+
+if($escuela == "CETIS 107"){
+    echo "<div style='background-color:blue;'>CETIS 107</div>";
+}else if($escuela == "CBTIS 224"){
+        echo "<div style='background-color:red;'>CBTIS 224</div>";
+     } else if($escuela == "COBAES"){
+            echo "<div style='background-color:green;'>COBAES</div>";
+
+    }
+     
+    
+
+
+?>
